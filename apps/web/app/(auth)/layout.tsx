d108ca2a80b13b6c2691098c5fd0da597bb77471
Dishant Radadiya <dishantradadiya3036@gmail.com>
2026-05-27 22:25:45 +0530

    build auth layout

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+import { Logo } from "@/components/logo"
+
+const AuthLayout = ({children} : {children: React.ReactNode}) => {
+    return <main className="h-screen w-full flex justify-center items-center">
+        
+        <Logo classname="fixed top-4 left-6"/>
+        {children}</main>
+}
+
+export default AuthLayout
