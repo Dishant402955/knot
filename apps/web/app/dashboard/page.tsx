b70920cec37ea8bc221eb16678e7c30b9c05268c
Dishant Radadiya <dishantradadiya3036@gmail.com>
2026-05-28 01:29:33 +0530

    build dashboard page

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,5 +1,76 @@
+import { Card, CardContent, CardDescription, CardFooter } from "@/components/ui/card"
+
 const DashboardPage = () => {
-    return <div className="h-full w-full flex justify-center items-center">Hello</div>
+    return <div className="h-full w-full flex justify-center pl-20 py-10 flex-col">
+        <div className="flex-col space-y-5 mb-10">
+            <p>Recent Videos</p>
+            <div className="flex justify-between pr-40">
+                <Card className="w-80">
+                    <CardContent>
+                        First Video
+                    </CardContent>
+                    <CardDescription className="px-4">
+                        this is first video
+                    </CardDescription>
+                    <CardFooter>
+                        this is footer
+                    </CardFooter>
+                </Card>
+                                <Card className="w-80">
+                    <CardContent>
+                        First Video
+                    </CardContent>
+                    <CardDescription className="px-4">
+                        this is first video
+                    </CardDescription>
+                    <CardFooter>
+                        this is footer
+                    </CardFooter>
+                </Card>
+                                <Card className="w-80">
+                    <CardContent>
+                        First Video
+                    </CardContent>
+                    <CardDescription className="px-4">
+                        this is first video
+                    </CardDescription>
+                    <CardFooter>
+                        this is footer
+                    </CardFooter>
+                </Card>
+            </div>
+        </div>
+
+        <div className="flex-col space-y-5 mt-10">
+            <p>Recent Folders</p>
+                        <div className="flex justify-between pr-40">
+                <Card className="w-80">
+                    <CardContent>
+                        First Video
+                    </CardContent>
+                    <CardDescription className="px-4">
+                        this is first video
+                    </CardDescription>
+                </Card>
+                                <Card className="w-80">
+                    <CardContent>
+                        First Video
+                    </CardContent>
+                    <CardDescription className="px-4">
+                        this is first video
+                    </CardDescription>
+                </Card>
+                                <Card className="w-80">
+                    <CardContent>
+                        First Video
+                    </CardContent>
+                    <CardDescription className="px-4">
+                        this is first video
+                    </CardDescription>
+                </Card>
+            </div>
+        </div>
+    </div>
 }
 
 export default DashboardPage
